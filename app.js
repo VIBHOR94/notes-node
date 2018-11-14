aa079dac7956ae0648b04527a34a36a5cbd722e7
VIBHOR94 <vibhorgupta2008@aol.in>
2018-11-15 03:24:21 +0530

    Add examples of lodash functionality

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ console.log('Starting app.js')
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes.js')
 
-var res = notes.addNote();
-console.log(res)
+// console.log(_.isString(true))
+// console.log(_.isString('Andrew'))
+var filteredArray = _.uniq(['Andrew', 1, 'Andrew', 1, 2, 3, 4])
+console.log(filteredArray)
 
-console.log(notes.addNumbers(9, -2))
+// console.log(notes.addNumbers(9, -2))
 
 // var user = os.userInfo()
 

88d852c7d8763bf9c12b2d132e5e93651e3bd5ef
VIBHOR94 <vibhorgupta2008@aol.in>
2018-11-15 14:26:11 +0530

    Read arguments passed through command line

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const fs = require('fs')
 const _ = require('lodash')
 
 const notes = require('./notes.js')
+
+var command = process.argv[2]
+console.log('Command:', command)
+
+if (command === 'add') {
+  console.log('Add new note.')
+} else if (command === 'list') {
+  console.log('Listing all nodes.')
+} else if (command === 'read') {
+  console.log('Reading a node.')
+} else if (command === 'remove') {
+  console.log('Removing node.')
+} else {
+  console.log('Command not recognized.')
+}
